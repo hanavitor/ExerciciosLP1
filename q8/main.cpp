84c89852f3009d50602c80d7776bcb49f5f011b7
hanavitor <hanaelvitor@gmail.com>
2018-08-16 22:42:27 -0300

    m

--- q8/main.cpp
+++ q8/main.cpp
@@ -11,10 +11,10 @@ int main(){
     c1.setPagamentos(Pagamentos(1200,"carlos"),0);
     c1.setPagamentos(Pagamentos(3200,"pedro"),1);
 
-    cout << c1.existePagamento("joao") << endl;
-    cout << c1.existePagamento("carlos") << endl;
-    cout << c1.getPagamento(1) << endl;
-    cout << c1.calculaPagamentos() << endl;
+    cout << "Existe pagamento(1/0): " << c1.existePagamento("joao") << endl;
+    cout << "Existe pagamento(1/0): " << c1.existePagamento("carlos") << endl;
+    cout << "Pagamento indice 1: " << c1.getPagamento(1) << endl;
+    cout << "Total de pagamentos: " << c1.calculaPagamentos() << endl;
 
     return 0;
 }
